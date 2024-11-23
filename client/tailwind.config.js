5ed392d786405aa8106cb7b859d9eff43b39f8b5
Specticall <josephyusmita@gmail.com>
2024-11-23 20:01:38 +0700

    frontend setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#131313",
+        secondary: "#1E1F21",
+        border: "#2A2B35",
+        accent: "#CA4141",
+        light: "#BFC2E4",
+        background: "#0D0D0F",
+      },
+    },
+    screens: {
+      "2xl": { max: "1440px" },
+      xl: { max: "1200px" },
+      lg: { max: "900px" },
+      md: { max: "768px" },
+      sm: { max: "600px" },
+      xs: { max: "450px" },
+    },
+  },
+  plugins: [],
+};
