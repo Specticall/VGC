b6b3c6dd36103bb8ebd9bd5255d14f3d376b7351
nocturnalnerds <133097737+nocturnalnerds@users.noreply.github.com>
2024-11-29 23:48:56 +0700

    Temp commit

---
+++ server/src/controllers/cinemaController.ts
@@ -0,0 +1,20 @@
+import { successRes } from "@/utils";
+import { AppError } from "@/utils/AppError";
+import { STATUS } from "@/utils/statusCodes";
+import { PrismaClient } from "@prisma/client";
+import { RequestHandler } from "express";
+
+const prisma = new PrismaClient();
+
+// get list of cinema aja
+export const getCinema: RequestHandler = async (request, response, next) => {
+	try {
+		const cinemas = await prisma.cinema.findMany();
+		if (!cinemas) {
+			throw new AppError("No Cinema Found", STATUS.NOT_FOUND);
+		}
+		return successRes(response, cinemas);
+	} catch (e) {
+		next(e);
+	}
+};
--- server/src/controllers/scheduleController.ts
+++ server/src/controllers/scheduleController.ts
@@ -2,51 +2,108 @@ import { successRes } from "@/utils";
 import { AppError } from "@/utils/AppError";
 import { STATUS } from "@/utils/statusCodes";
 import { PrismaClient } from "@prisma/client";
-import { count } from "console";
 import { RequestHandler } from "express";
+import { getRooms } from "./roomController";
+import { Decimal } from "@prisma/client/runtime/library";
 
 const prisma = new PrismaClient();
 
 export const getSchedules: RequestHandler = async (request, response, next) => {
-  try {
-    const movieId = request.query.movieId as string | undefined;
+	try {
+		const movieId = request.query.movieId as string | undefined;
 
-    const schedules = await prisma.schedule.findMany({
-      where: {
-        MovieId: movieId,
-      },
-      include: {
-        movie: {
-          select: {
-            Title: true,
-            Poster: true,
-            Tagline: true,
-          },
-        },
-        room: {
-          select: {
-            Name: true,
-            cinema: {
-              select: {
-                Name: true,
-              },
-            },
-          },
-        },
-        _count: {
-          select: {
-            reservations: true,
-          },
-        },
-      },
-      take: 100,
-    });
-    if (!schedules) {
-      throw new AppError("No schedules found", STATUS.NOT_FOUND);
-    }
+		const schedules = await prisma.schedule.findMany({
+			where: {
+				MovieId: movieId,
+			},
+			include: {
+				movie: {
+					select: {
+						Title: true,
+						Poster: true,
+						Tagline: true,
+					},
+				},
+				room: {
+					select: {
+						Name: true,
+						cinema: {
+							select: {
+								Name: true,
+							},
+						},
+					},
+				},
+				_count: {
+					select: {
+						reservations: true,
+					},
+				},
+			},
+			take: 100,
+		});
+		if (!schedules) {
+			throw new AppError("No schedules found", STATUS.NOT_FOUND);
+		}
 
-    return successRes(response, schedules);
-  } catch (error) {
-    next(error);
-  }
+		return successRes(response, schedules);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const postSchedules: RequestHandler = async (
+	request,
+	response,
+	next
+) => {
+	// StartTime       DateTime
+	// EndTime         DateTime
+	// RoomId          String
+	// MovieId         String
+	// CreatedAt       DateTime     @default(now())
+	// UpdatedAt       DateTime     @updatedAt
+	// room            Room          @relation(fields: [RoomId], references: [RoomId])
+	// movie           Movie         @relation(fields: [MovieId], references: [MovieId])
+	// reservations    Reservation[]
+	try {
+		const { roomId, movieId, ticketPrice, startDate, time, duration } =
+			request.body;
+
+		if (!roomId) {
+			throw new AppError(
+				"Room not found in the specified cinema",
+				STATUS.BAD_REQUEST
+			);
+		}
+		if (!ticketPrice) {
+			throw new AppError("Price Not Found", STATUS.BAD_REQUEST);
+		}
+		if (!startDate) {
+			throw new AppError("Date Not Found", STATUS.BAD_REQUEST);
+		}
+
+		if (!movieId) {
+			throw new AppError("Movie Not Found", STATUS.BAD_REQUEST);
+		}
+
+		if (!time || !Array.isArray(time)) {
+			throw new AppError("Time should be an array", STATUS.BAD_REQUEST);
+		}
+		// const schedules = await Promise.all(
+		// 	time.map(async (t: string) => {
+		// 		return await prisma.schedule.create({
+		// 			data: {
+		// 				RoomId: roomId,
+		// 				Date: date,
+		// 				Time: t,
+		// 			},
+		// 		});
+		// 	})
+		// );
+
+		// return successRes(response, schedules);
+	} catch (error) {
+		next(error);
+	}
 };
