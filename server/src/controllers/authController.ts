26f7a16c8c594c82d326cde3285d0ce9aab4867e
nocturnalnerds <133097737+nocturnalnerds@users.noreply.github.com>
2024-11-30 23:41:31 +0700

    login/register

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -2,6 +2,7 @@ import { createJWT, successRes, AppError, STATUS } from "@/utils";
 import { PrismaClient } from "@prisma/client";
 import axios from "axios";
 import { RequestHandler } from "express";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
@@ -63,16 +64,142 @@ export const loginWithGoogle: RequestHandler = async (
 	}
 };
 
-export const Login: RequestHandler = async (request, response, next) => {
+export const login: RequestHandler = async (request, response, next) => {
 	try {
-		const access_token = request.body["access_token"];
+		const { access_token, email, password } = request.body;
 		if (!access_token) {
 			throw new AppError(
 				"Token is missing in the request body",
 				STATUS.BAD_REQUEST
 			);
 		}
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		if (email.includes("'")) {
+			throw new AppError(
+				"AOWKAOWKAOWK MAU SQL INJECT YA PUKI?",
+				STATUS.BAD_REQUEST
+			);
+		}
+		if (!emailRegex.test(email)) {
+			throw new AppError("Email format is invalid", STATUS.BAD_REQUEST);
+		}
+		if (!email) {
+			throw new AppError(
+				"email is missing in the request body",
+				STATUS.BAD_REQUEST
+			);
+		}
+		if (!password) {
+			throw new AppError(
+				"password is missing in the request body",
+				STATUS.BAD_REQUEST
+			);
+		}
+		const userData = await prisma.user.findUnique({
+			where: { Email: email },
+		});
+
+		if (!userData || !(await bcrypt.compare(password, userData.Password))) {
+			throw new AppError("Invalid email or password", STATUS.UNAUTHORIZED);
+		}
+
+		const jwtToken = createJWT(userData);
+		if (!jwtToken) {
+			throw new AppError(
+				"Something went wrong while trying to create the token",
+				STATUS.INTERNAL_SERVER_ERROR
+			);
+		}
+
+		return successRes(response, {
+			token: jwtToken,
+			userData: userData,
+		});
 	} catch (error) {
 		next(error);
+	}
+};
+
+export const register: RequestHandler = async (request, response, next) => {
+	try {
+		const { access_token, name, email, password, age } = request.body;
+		if (!access_token) {
+			throw new AppError(
+				"Token is missing in the request body",
+				STATUS.BAD_REQUEST
+			);
+		}
+		if (!name) {
+			throw new AppError(
+				"Name is missing in the request body",
+				STATUS.BAD_REQUEST
+			);
+		}
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		if (email.includes("'")) {
+			throw new AppError(
+				"AOWKAOWKAOWK MAU SQL INJECT YA PUKI?",
+				STATUS.BAD_REQUEST
+			);
+		}
+		if (!emailRegex.test(email)) {
+			throw new AppError("Email format is invalid", STATUS.BAD_REQUEST);
+		}
+		if (!email) {
+			throw new AppError(
+				"Email is missing in the request body",
+				STATUS.BAD_REQUEST
+			);
+		}
+		if (!password) {
+			throw new AppError(
+				"password is missing in the request body",
+				STATUS.BAD_REQUEST
+			);
+		}
+		if (!age) {
+			throw new AppError(
+				"Age is missing in the request body",
+				STATUS.BAD_REQUEST
+			);
+		}
+		const existingUser = await prisma.user.findUnique({
+			where: { Email: email },
+		});
+
+		if (existingUser) {
+			throw new AppError(
+				"User with this email already exists",
+				STATUS.FORBIDDEN
+			);
+		}
+
+		const hashedPassword = await bcrypt.hash(password, 12);
+
+		const newUser = await prisma.user.create({
+			data: {
+				Name: name,
+				Email: email,
+				Password: hashedPassword,
+				ProfilePicture: "",
+				Age: age,
+				Role: "USER",
+			},
+		});
+
+		const jwtToken = createJWT(newUser);
+		if (!jwtToken) {
+			throw new AppError(
+				"Something went wrong while trying to create the token",
+				STATUS.INTERNAL_SERVER_ERROR
+			);
+		}
+
+		return successRes(response, {
+			token: jwtToken,
+			userData: newUser,
+		});
+	} catch (e) {
+		next(e);
 	}
 };
