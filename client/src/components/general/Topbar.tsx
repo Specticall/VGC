fdeb188d424bf612e8d7655cdb6acc4ae3517431
nocturnalnerds <133097737+nocturnalnerds@users.noreply.github.com>
2024-12-05 02:14:23 +0700

    ganti biar gak static

--- client/src/components/general/Topbar.tsx
+++ client/src/components/general/Topbar.tsx
@@ -2,21 +2,21 @@ import useUserQuery from "@/hooks/queries/useUserQuery";
 import SearchPopup from "./SearchPopup";
 
 type Props = {
-  searchBar?: boolean;
+	searchBar?: boolean;
 };
 
 export default function Topbar({ searchBar }: Props) {
-  const { userData } = useUserQuery();
-  return (
-    <nav className="text-light grid grid-cols-[auto_1fr] px-8 py-3 items-center bg-primary border-b border-border">
-      {searchBar && <SearchPopup />}
-      <div className="flex justify-self-end items-center gap-4">
-        <p className="justify-self-end">Welcome, Joseph</p>
-        <img
-          src={userData?.ProfilePicture}
-          className="w-12 aspect-square rounded-full bg-gray"
-        ></img>
-      </div>
-    </nav>
-  );
+	const { userData } = useUserQuery();
+	return (
+		<nav className="text-light grid grid-cols-[auto_1fr] px-8 py-3 items-center bg-primary border-b border-border">
+			{searchBar && <SearchPopup />}
+			<div className="flex justify-self-end items-center gap-4">
+				<p className="justify-self-end">Welcome, {userData?.Name}</p>
+				<img
+					src={userData?.ProfilePicture}
+					className="w-12 aspect-square rounded-full bg-gray"
+				></img>
+			</div>
+		</nav>
+	);
 }
